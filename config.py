da8356f9b5465fccb933df4810799f0522491ae0
athrowaway2021 <76710841+athrowaway2021@users.noreply.github.com>
2020-12-29 13:37:54 +0000

    Added API URLs for listing and issue info

--- config.py
+++ config.py
@@ -7,7 +7,11 @@ if (AUTH_TOKEN == "token goes here"):
     exit()
 
 API_DOWNLOAD_URL = "https://cmx-secure.comixology.com/ios/api/com.iconology.android.Comics/3.9.7/?deviceType=tablet&lang=en&store=US&action=getUserPurchase"
+API_ISSUE_URL = "http://digital.comixology.com/ios/api/com.iconology.android.Comics/3.9.7/?deviceType=tablet&lang=en&store=US&action=getIssueSummaries"
+API_LIST_URL = "https://cmx-secure.comixology.com/ios/api/com.iconology.android.Comics/3.9.7/?deviceType=tablet&lang=en&store=US&action=getPurchaseTransactions"
+
 API_HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36",
     "x-api-version" : "3.9.7",
     "x-client-application" : "com.comixology.comics",
     "x-currency" : "USD_US",

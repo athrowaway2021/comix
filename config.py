b95282536c7a75abc8bfb0102d5b7c9be92efbd1
athrowaway2021 <76710841+athrowaway2021@users.noreply.github.com>
2021-04-16 13:51:42 +0100

    Updated headers

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
-# aToken cookie in the browser site (region does not matter)
-# get it from the application tab in the dev tools or from a request in the network profiler
+# aToken cookie in the comixology app
 AUTH_TOKEN = "token goes here"
 
 if (AUTH_TOKEN == "token goes here"):
@@ -11,10 +10,6 @@ API_ISSUE_URL = "http://digital.comixology.com/ios/api/com.iconology.android.Com
 API_LIST_URL = "https://cmx-secure.comixology.com/ios/api/com.iconology.android.Comics/3.9.7/?deviceType=tablet&lang=en&store=US&action=getPurchaseTransactions"
 
 API_HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36",
-    "x-api-version" : "3.9.7",
-    "x-client-application" : "com.comixology.comics",
-    "x-currency" : "USD_US",
-    "x-region" : "US",
-    "x-user-token" : "lwa-|" + AUTH_TOKEN
+    "User-Agent": "Comics/3.10.17[3.10.17.310418] Google/10",
+    "x-client-application" : "com.comixology.comics"
 }
